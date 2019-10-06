469f3b4267e48c16d3345de37a65a857a8d25d79
jay881101 <395681340@qq.com>
2019-10-06 12:08:34 +0800

    first test

---
+++ src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.example.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+    /**
+     * demo-test
+     * http://127.0.0.1:9005/config-demo.properties
+     * word: hello world
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+
+}
